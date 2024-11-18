95e39209459ae83bcc26112170670fb6147b4b81
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2024-11-18 01:53:52 +0000

    database unsecured queries
    all queries are now unsecure, data implemented directly into the query.

--- models/userModel.js
+++ models/userModel.js
@@ -5,10 +5,10 @@ const User = {
         console.log('Creating user...')
         const {first_name, last_name, username, email, password} = data;
         console.log(data);
-        db.run('INSERT INTO users (first_name, last_name, username, password, email) VALUES (?,?,?,?,?)', [first_name, last_name, username, password, email], callback);
+        db.run('INSERT INTO users (',first_name,',', last_name,',', username,',', password,',', email,')', callback);
     },
     findByUsername: (username, callback) => {
-        db.get('SELECT * FROM users WHERE username = ?', [username], callback);
+        db.get('SELECT * FROM users WHERE username = ', username, ')', callback);
 },
 };
 
